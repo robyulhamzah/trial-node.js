6b86a269b019f72cd657afaad179ea23b98c2d2d
robyulhamzah <34570203+robyulhamzah@users.noreply.github.com>
2026-05-17 22:25:56 +0700

    Refactor user retrieval logic in server.js

--- server.js
+++ server.js
@@ -15,13 +15,49 @@ app.get("/", (req, res) => {
 
 
 // GET USERS
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+// HOME
+app.get("/", (req, res) => {
+  res.send("Server Node.js berjalan!");
+});
+
+
+// GET USERS
 app.get("/users", (req, res) => {
 
-  const data = fs.readFileSync("users.json");
+  try {
 
-  const users = JSON.parse(data);
+    const filePath = path.join(__dirname, "users.json");
 
-  res.json(users);
+    const data = fs.readFileSync(filePath);
+
+    const users = JSON.parse(data);
+
+    res.json(users);
+
+  } catch (err) {
+
+    res.status(500).json({
+      error: err.message
+    });
+
+  }
+
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
